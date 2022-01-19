bdc6fe72af9beb7540653fc1684e4097e2fc8a6a
Beorge <beorgerer64@gmail.com>
2022-01-19 15:40:18 +0500

    add icon and title

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Header from '../Header/Headet';
 import Main from '../Main/Main';
 
 const App: React.FC = () => {
+	useEffect(() => {
+		document.title = 'LA Helper'
+	})
 	return (
 		<div className='root'>
 			<Header />
-			<Main />
-			
+			<Main />	
 		</div>
 	);
 }
